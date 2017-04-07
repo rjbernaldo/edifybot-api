6d8a9bac927db5bade9a9f2fc66b5933ed19f432
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-07 18:46:10 +0800

    Update current state of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  # facebook api
   get 'bot/webhook' => 'bot#verify_token'
   post 'bot/webhook' => 'bot#receive_data'
 
+  # report
   get 'users/:sender_id' => 'users#show'
-
+  
+  # expense list
   get 'users/:sender_id/search' => 'expenses#search'
   get 'users/:sender_id/expenses' => 'expenses#index'
+  
+  # expense
   get 'users/:sender_id/expenses/:id' => 'expenses#show'
-  # post 'users/:sender_id/expenses/:id' => 'expenses#create'
   patch 'users/:sender_id/expenses/:id' => 'expenses#update'
   delete 'users/:sender_id/expenses/:id' => 'expenses#destroy'
 end
